deb3fea3f8b3b929b4ca485fc0c44654ff39c412
desser2002 <oxxxymironushe@gmail.com>
2024-10-14 13:23:29 +0200

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
             src="https://nextjs.org/icons/globe.svg"
             alt="Globe icon"
             width={16}
-            height={16}
+            height={17}
           />
           Go to nextjs.org →
         </a>
