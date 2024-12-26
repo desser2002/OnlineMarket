c0214cc158ffe78d0744fd0474b4a76d96ad59ce
desser2002 <oxxxymironushe@gmail.com>
2024-12-26 12:14:35 +0100

    payment

--- src/types/Order.ts
+++ src/types/Order.ts
@@ -1,4 +1,3 @@
-// OrderType.ts
 export type Order = {
     id: string;
     date: string;
@@ -6,7 +5,3 @@ export type Order = {
     status: 'Pre-order' | 'In transit' | 'Confirmed' | 'Cancelled';
 };
 
-export type OrderActionProps = {
-    onCancel: () => void;
-    onViewDetails: () => void;
-};
